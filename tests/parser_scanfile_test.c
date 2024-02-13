217daf8e8f44671e55d4147bef8c067831b6bb40
skantor-mtb <139501569+skantor-mtb@users.noreply.github.com>
2024-02-13 16:10:26 +0000

    Added messages to tests on start and pass

--- tests/parser_scanfile_test.c
+++ tests/parser_scanfile_test.c
@@ -3,9 +3,11 @@
 
 int main(void)
 {
+    printf("======\n======\nTesting scan_file()\n======\n======\n");
     // Test that a single, valid instruction in a file returns a correct result
 
     // Commands without argument should return 1, set the argument of instruction to -1, and store the text before the single space in instruction->command
+    printf("Testing single_good_noarg\n");
     FILE *single_good_noarg = fopen("scanfile_single_good_cmd_noarg", "r");
     instruction *result = calloc(1, sizeof(instruction));
     int matches = scan_file(single_good_noarg, result);
@@ -28,9 +30,11 @@ int main(void)
         return 0;
     }
     free(result);
+    printf("Passed single_good_noarg\n");
 
     // Commands with argument should return a number > 1, set instruction->argument to the integer after the single space, and the command should be set to the text in the file before the whitespace
     // This only happens with push
+    printf("Testing single_good_arg\n");
     FILE *single_good_arg = fopen("scanfile_single_good_cmd_arg", "r");
     result = malloc(sizeof(instruction));
     matches = scan_file(single_good_arg, result);
@@ -53,8 +57,10 @@ int main(void)
         return 0;
     }
     free(result);
+     printf("Passed single_good_arg\n");
 
     // Empty file should return -1
+     printf("Testing empty_file\n");
     FILE *empty = fopen("scanfile_empty_file", "r");
     result = malloc(sizeof(instruction));
     matches = scan_file(empty, result);
@@ -73,6 +79,7 @@ int main(void)
     free(result);
 
     // Test that multiple, valid instructions in a file returns a correct result
+    printf("Testing multi_good_noarg\n");
     FILE *multiple_good_cmd_noarg = fopen("scanfile_multi_good_cmd_noarg", "r");
     int count = 1;
     const int FILE_LEN = 10;
@@ -118,8 +125,10 @@ int main(void)
         count++;
     }
     free(result);
+    printf("Passed multi_good_noarg\n");
 
     // Test that a single instruction, that is in the correct form, but not a valid instruction still returns a command
+    printf("Testing correct_form_no_cmd_noarg\n");
     FILE *correct_form_no_cmd = fopen("scanfile_correct_form_no_cmd", "r");
     result = calloc(1, sizeof(instruction));
     matches = scan_file(correct_form_no_cmd, result);
@@ -136,16 +145,20 @@ int main(void)
         return 0;
     }
     free(result);
+     printf("Passed correct_form_no_cmd_noarg\n");
 
     // Test that a single instruction in an incorrect format returns an error
 
     // Command too long (more than 6 chars)
+    //This should truncate the command, but otherwise act as though nothing is wrong
+     printf("Testing cmd_too_long\n");
     FILE *cmd_too_long = fopen("scanfile_cmd_too_long", "r");
     result = calloc(1, sizeof(instruction));
     matches = scan_file(cmd_too_long, result);
-    if (matches != -1)
+    if (matches != 1)
     {
-        printf("scan_file() test: Failed command too long.\nscan_file returned %d, should have returned -1\n", matches);
+        printf("scan_file() test: Failed command too long.\nscan_file returned %d, should have returned 1\n", matches);
+        printf("%s, %d\n", result->command, result->arg);
         free(result);
         return 0;
     }
@@ -155,10 +168,18 @@ int main(void)
         free(result);
         return 0;
     }
+    if (strcmp(result->command, "aaaaad") != 0)
+    {
+         printf("scan_file() test: Failed command too long. Command should have truncated to \"aaaaad\", instead is %s\n", result->command);
+        free(result);
+        return 0;
+    }
     free(result);
+    printf("Passed cmd_too_long\n");
 
     // Command too short (Essentially, no command)
-    FILE *cmd_too_short = fopen("scanfile_cmd_too_short", "r");
+    printf("Testing cmd_too_short\n");
+    FILE *cmd_too_short = fopen("scanfile_too_short", "r");
     result = calloc(1, sizeof(instruction));
     matches = scan_file(cmd_too_short, result);
     if (matches != -1)
@@ -174,8 +195,10 @@ int main(void)
         return 0;
     }
     free(result);
+    printf("Passed cmd_too_short\n");
 
     // Too much whitespace
+    printf("Testing too_much_whitespace\n");
     FILE *too_much_space = fopen("scanfile_too_much_whitespace", "r");
     result = calloc(1, sizeof(instruction));
     matches = scan_file(too_much_space, result);
@@ -192,8 +215,10 @@ int main(void)
         return 0;
     }
     free(result);
+    printf("Passed too_much_whitespace\n");
 
     // Too little whitespace
+    printf("Testing too_little_whitespace\n");
     FILE *too_little_space = fopen("scanfile_too_little_whitespace", "r");
     result = calloc(1, sizeof(instruction));
     matches = scan_file(too_little_space, result);
@@ -210,8 +235,10 @@ int main(void)
         return 0;
     }
     free(result);
+    printf("Testing too_little_whitespace\n");
 
     // Too many arguments
+    printf("Testing too_many_args\n");
     FILE *too_many_args = fopen("scanfile_too_,many_args", "r");
     result = calloc(1, sizeof(instruction));
     matches = scan_file(too_many_args, result);
@@ -228,4 +255,6 @@ int main(void)
         return 0;
     }
     free(result);
+    printf("Passed too_many_args\n");
+      printf("======\n======\nPassed scan_file()\n======\n======\n");
 }
